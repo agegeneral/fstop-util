5618249565f34d0218397977a19028f5155bd17b
andy <andy.udp.ip@gmail.com>
2017-06-20 19:24:23 +0800

    add slf4j function and testing

--- src/main/java/fstop/App.java
+++ src/main/java/fstop/App.java
@@ -1,13 +1,21 @@
 package fstop;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World  --!" );
+        logger.debug("aaaaaaaaaaaaaaaaaaa");
     }
 }
